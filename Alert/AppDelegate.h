b1689a335cbce6df5f406e1733345678bace7695
Tecksky Techonologies <tecksky@Teckskys-Mac-mini.local>
2016-11-11 17:27:45 +0530

    Initial Commit

---
+++ Alert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alert
+//
+//  Created by Tecksky Techonologies on 11/11/16.
+//  Copyright © 2016 Softranz Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
